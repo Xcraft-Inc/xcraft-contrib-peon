692a3a0a480fdf6150f4bfa93fd3ddcdb088a77a
Mathieu Schroeter <schroeter@epsitec.ch>
2014-07-09 16:32:06 +0200

    Add more stubs with zogPeon. Now the main module is able to return an object with all available backends.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var moduleName = 'peon';
+
+var path  = require ('path');
+var zogFs = require ('zogFs');
+
+var backendsRoot = path.join (__dirname, 'backends');
+var backends = {};
+var backendsType = zogFs.lsdir (backendsRoot);
+
+backendsType.forEach (function (type)
+{
+  var backendsCmd = zogFs.ls (path.join (backendsRoot, type), /\.js$/);
+
+  backends[type] = {};
+  backendsCmd.forEach (function (cmd)
+  {
+    var cmdName = cmd.replace (/\.js$/, '');
+    backends[type][cmdName] = require (path.join (backendsRoot, type, cmd));
+  });
+});
+
+module.exports = backends;
