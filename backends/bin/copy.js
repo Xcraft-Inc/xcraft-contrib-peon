f331f2b6539557a5c47f0c7c08b86e7cc8f23eca
Mathieu Schroeter <schroeter@epsitec.ch>
2014-07-10 11:28:38 +0200

    Implement the copy backend for file:// and use it with zogMake.

--- backends/bin/copy.js
+++ backends/bin/copy.js
@@ -1,6 +1,31 @@
 'use strict';
 
-module.exports = function (srcDir)
+module.exports = function (srcUri, destPath)
 {
-  
+  var path        = require ('path');
+  var url         = require ('url');
+  var zogFs       = require ('zogFs');
+  var zogPlatform = require ('zogPlatform');
+
+  var uriObj = url.parse (srcUri);
+
+  switch (uriObj.protocol)
+  {
+  case 'http:':
+  case 'https:':
+    console.log (uriObj.protocol + ' support not implemented');
+    break;
+
+  case 'file:':
+    var srcPath = uriObj.pathname;
+    if (zogPlatform.getOs () === 'win')
+      srcPath = path.normalize (srcPath.replace (/^\/([a-z]:)/, '$1'));
+
+    zogFs.cpdir (srcPath, destPath);
+    break;
+
+  default:
+    console.log (uriObj.protocol + ' not supported');
+    break;
+  }
 };
