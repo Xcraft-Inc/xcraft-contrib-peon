243643d9793a5feb2699b6748f15d2171ed98825
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-07-29 18:58:15 +0200

    We consider the URI only when the cache/ directory is not available, then we spawn the installer immediately.

--- backends/bin/exec.js
+++ backends/bin/exec.js
@@ -1,25 +1,38 @@
 'use strict';
 
+var path  = require ('path');
 var utils = require ('../../utils.js');
+
+var spawn = function (share, extra, callbackDone)
+{
+  if (!extra.hasOwnProperty ('bin'))
+  {
+    callbackDone (true);
+    return;
+  }
+
+  var zogProcess = require ('zogProcess');
+
+  var bin = path.join (share, extra.bin);
+  var args = extra.args.split (' ');
+
+  console.log ('spawn %s %s', bin, extra.args);
+
+  zogProcess.spawn (bin, args, callbackDone);
+};
 
 module.exports = function (srcUri, root, share, extra, callbackDone)
 {
-  var srcFile = utils.fileFromUri (srcUri, share, function (file)
+  var fs = require ('fs');
+
+  if (fs.existsSync (path.join (share, 'cache')))
   {
-    if (!extra.hasOwnProperty ('bin'))
-    {
-      callbackDone (true);
-      return;
-    }
+    spawn (share, extra, callbackDone);
+    return;
+  }
 
-    var path       = require ('path');
-    var zogProcess = require ('zogProcess');
-
-    var bin = path.join (file, extra.bin);
-    var args = extra.args.split (' ');
-
-    console.log ('spawn %s %s', bin, extra.args);
-
-    zogProcess.spawn (bin, args, callbackDone);
+  var srcFile = utils.fileFromUri (srcUri, share, function (dir)
+  {
+    spawn (dir, extra, callbackDone);
   });
 };
