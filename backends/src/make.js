dad735d2ed353d114044f1efd5ce40fe98f66e1c
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-08-19 11:40:03 +0200

    First step in order to implement the 'make' rule. It downloads the sources if necessary. Note that wpkg still refuse to create a package like that.

--- backends/src/make.js
+++ backends/src/make.js
@@ -1,6 +1,33 @@
 'use strict';
 
+var path  = require ('path');
+var utils = require ('../../utils.js');
+
+var make = function (share, extra, callbackDone)
+{
+  if (!extra.hasOwnProperty ('location'))
+  {
+    callbackDone (true);
+    return;
+  }
+
+  callbackDone (false);
+};
+
 module.exports = function (srcUri, root, share, extra, callbackDone)
 {
-  callbackDone (false);
+  var fs = require ('fs');
+
+  var cache = path.join (share, 'cache');
+
+  if (fs.existsSync (cache))
+  {
+    make (share, extra, callbackDone);
+    return;
+  }
+
+  utils.fileFromUri (srcUri, share, function (src)
+  {
+    make (share, extra, callbackDone);
+  });
 };
