30ec1f6101c5ffc120ead8ce45e14ef320275f94
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-04-22 06:30:30 +0200

    Add a trivial implementation of a first sln backend.
    Without no nuget and xbuild handling.

--- backends/src/sln.js
+++ backends/src/sln.js
@@ -3,7 +3,21 @@
 var base = require ('../../lib/base.js');
 
 var msbuild = function (cache, extra, callback) {
-  callback ('sln is a stub');
+  var xProcess = require ('xcraft-core-process') ();
+
+  console.log ('cache: ' + cache + ' ' + JSON.stringify (extra));
+
+  var makeBin = 'msbuild'; /* FIXME: or xbuild if msbuild is not found */
+  var args = [
+    cache
+  ];
+
+  if (extra.args) {
+    args = args.concat (extra.args);
+  }
+
+  console.log (makeBin + ' ' + args.join (' '));
+  xProcess.spawn (makeBin, args, {}, callback);
 };
 
 module.exports = function (getObj, root, share, extra, callback) {
