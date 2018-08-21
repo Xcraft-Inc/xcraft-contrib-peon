ad8d0d23f3a434b2bcaed3ddd196ce447ba6cfea
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-08-21 11:47:46 +0200

    Rename sln src type to msbuild-(full|core)
    It handles msbuild, dotnet msbuild and xcraft.

---
+++ backends/src/msbuild-core.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const base = require('../../lib/base.js');
+const msbuild = require('../../lib/backends/src/msbuild.js')(true);
+
+module.exports = function(getObj, root, share, extra, response, callback) {
+  extra._rulesTypeDir = __dirname;
+  base.onlyBuild(getObj, root, share, extra, response, callback, function(
+    data,
+    callback
+  ) {
+    msbuild(data.fullLocation, data.extra, response, callback);
+  });
+};
