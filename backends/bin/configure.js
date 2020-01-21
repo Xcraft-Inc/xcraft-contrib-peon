8f900f0736d17e512f6b3c85288ee054d7432c52
Mathieu Schroeter <mathieu@schroetersa.ch>
2020-01-21 14:35:12 +0100

    Add support for postinst and prerm scripts with configure packages

--- backends/bin/configure.js
+++ backends/bin/configure.js
@@ -1,6 +1,41 @@
 'use strict';
 
-var base = require('../../lib/base.js');
+const watt = require('gigawatts');
+const base = require('../../lib/base.js');
+const helpers = require('xcraft-core-env/lib/helpers.js');
+
+const script = watt(function*(cache, extra, resp, next) {
+  resp.log.verb('cache: ' + cache + ' ' + JSON.stringify(extra));
+
+  const interpreter = require('../../lib/interpreter.js');
+
+  const currentDir = process.cwd();
+  process.chdir(cache);
+
+  const getArgs = args => {
+    if (!args) {
+      return;
+    }
+    if (Array.isArray(args)) {
+      return args.join(' ');
+    }
+    return args;
+  };
+
+  try {
+    if (extra.args.all) {
+      const script = helpers.injectPh(
+        getArgs(extra.args.all),
+        extra.distribution
+      );
+      yield interpreter.run(script, resp, next);
+    }
+  } catch (ex) {
+    throw ex;
+  } finally {
+    process.chdir(currentDir);
+  }
+});
 
 module.exports = function(getObj, root, share, extra, resp, callback) {
   base.onlyInstall(getObj, root, share, extra, resp, callback, function(
@@ -8,6 +43,6 @@ module.exports = function(getObj, root, share, extra, resp, callback) {
     callback
   ) {
     resp.log.info('configure package');
-    callback();
+    script(data.fullLocation, data.extra, resp, callback);
   });
 };
